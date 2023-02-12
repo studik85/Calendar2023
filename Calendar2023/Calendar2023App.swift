0581408f1df58af849bfcbb5c7c4ba492ee60039
Andrey <studenkoff@gmail.com>
2023-02-12 20:34:59 +0200

    Initial Commit

---
+++ Calendar2023/Calendar2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar2023App.swift
+//  Calendar2023
+//
+//  Created by Andrey Studenkov on 12.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Calendar2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
